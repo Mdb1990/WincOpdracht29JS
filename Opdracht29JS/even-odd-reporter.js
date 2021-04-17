8fef3fceca40ddd90b1330ef078245e92f86fae1
Marco de boer <76598975+Mdb1990@users.noreply.github.com>
2021-04-17 16:51:44 +0200

    Add files via upload

---
+++ Opdracht29JS/even-odd-reporter.js
@@ -0,0 +1,9 @@
+/** @format */
+
+for (i = 0; i <= 20; i++) {
+  if (i % 2) {
+    console.log(i + " is oneven");
+  } else {
+    console.log(i + " is even");
+  }
+}
---
+++ Opdracht29JS/multiplication-tables.js
@@ -0,0 +1,10 @@
+/** @format */
+
+for (i = 1; i <= 10; i++) {
+  let counter = 1;
+
+  for (counter = 1; counter <= 10; counter++) {
+    const sum = i * counter;
+    console.log(counter + " x " + i + " = " + sum);
+  }
+}
